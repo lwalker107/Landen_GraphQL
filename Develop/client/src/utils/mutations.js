147851d45550e563b3053896a3f150a81333017d
lwalker <landen@example.com>
2023-10-07 10:32:40 -0500

    finished mutations.js in the utils folder on the client side

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,53 @@
+import { gql } from '@apollo/client';
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookId: ID!, $description: String!, $title: String!, $authors: [String]!, $image: String!, $link: String!) {
+    saveBook(bookId: $bookId, description: $description, title: $title, authors: $authors, image: $image, link: $link) {
+      _id
+      username
+      bookCount
+      savedBooks {
+        bookId
+        title
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      bookCount
+      savedBooks {
+        bookId
+        title
+      }
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
