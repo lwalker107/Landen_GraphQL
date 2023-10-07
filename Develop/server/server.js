53977898efe7f4111d97c2969a797a48bf61d23e
lwalker <landen@example.com>
2023-10-07 16:25:19 -0500

    more bug fixes

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas')
 const path = require('path');
 const db = require('./config/connection');
 const server = new ApolloServer({
@@ -34,7 +35,7 @@ const startApolloServer = async () => {
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at httpe://localhost:${PORT}/graphql`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
   });
   });
 };
