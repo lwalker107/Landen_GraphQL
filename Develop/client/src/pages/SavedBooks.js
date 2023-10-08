6d6f858b50d1ac801cdf24d5ca83054fd9dca08d
lwalker <landen@example.com>
2023-10-08 03:00:13 -0500

    fixing loading screen for saving books

--- Develop/client/src/pages/SavedBooks.js
+++ Develop/client/src/pages/SavedBooks.js
@@ -16,7 +16,7 @@ import { removeBookId } from '../utils/localStorage';
 const SavedBooks = () => {
   const { loading, data } = useQuery(QUERY_ME);
 
-  const userData = data?.me;
+  const userData = data?.me || {};
 
   // use this to determine if `useEffect()` hook needs to run again
   const [deleteBook] = useMutation(REMOVE_BOOK);
@@ -54,12 +54,12 @@ const SavedBooks = () => {
       </div>
       <Container>
         <h2 className='pt-5'>
-          {userData.savedBooks.length
+          {userData.savedBooks?.length
             ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
             : 'You have no saved books!'}
         </h2>
         <Row>
-          {userData.savedBooks.map((book) => {
+          {userData.savedBooks?.map((book) => {
             return (
               <Col md="4">
                 <Card key={book.bookId} border='dark'>
